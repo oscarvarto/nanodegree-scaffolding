c1e58e431df1906da147ff5eb0865d90f22bf8b2
Oscar Vargas Torres <oscarvarto@protonmail.com>
2018-11-27 19:04:26 -0600

    First try with powershell scripts (not yet tested on Windows)

---
+++ run.ps1
@@ -0,0 +1,8 @@
+docker run -it --rm `
+-p 0.0.0.0:4000:4000 `
+--network=host `
+--name=scaffold `
+-v ~/.ivy2:/root/.ivy2  -v ~/.sbt:/root/.sbt `
+-w /nanodegree `
+-v $(pwd)/nanodegree:/nanodegree `
+nanodegree-scaffolding ./publish.sh -l
