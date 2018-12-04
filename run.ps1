daa7a249fe9cccefc2f8ee1e2ae2429670eda218
Oscar Vargas Torres <vargas.torres.oscar@gmail.com>
2018-12-04 17:03:28 -0600

    Windows Dev Env Working

--- run.ps1
+++ run.ps1
@@ -1,8 +1,7 @@
 docker run -it --rm `
--p 0.0.0.0:4000:4000 `
---network=host `
+-p 4000:4000 `
 --name=scaffold `
 -v ~/.ivy2:/root/.ivy2  -v ~/.sbt:/root/.sbt `
 -w /nanodegree `
--v $(pwd)/nanodegree:/nanodegree `
+-v $pwd/nanodegree:/nanodegree `
 nanodegree-scaffolding ./publish.sh -l
